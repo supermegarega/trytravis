2e37580cd65cd3ab727a8def33c130c993c348d2
Anton Russkov <supermegarega@mail.ru>
2018-04-12 17:32:52 +0300

    Add static inventory

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -69,3 +69,16 @@ resource "google_compute_firewall" "firewall_puma" {
   target_tags   = ["reddit-app"]
 }
 
+resource "google_compute_firewall" "firewall_nginx" {
+  name    = "allow-nginx-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
+
