5e36b36a611b651486725e03c20897914c31b36d
Anton Russkov <supermegarega@mail.ru>
2018-04-22 21:01:17 +0300

    Add docker-2 Infra

---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,41 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app-${count.index}"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+  count        = "${var.node_count}"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
